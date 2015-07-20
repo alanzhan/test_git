cc282241a8c272d70561a7a7e00881675fe08672
alanzhan <zx_hfut@qq.com>
2015-07-20 14:47:20 +0800

    first revise

--- A.java
+++ A.java
@@ -1,7 +1,7 @@
 public class A{
 	public static void main(String args[]){
 			int number=1;
-	    String name="alan";
+	    String name="Alan Zhang";
 	    B b =new B();
 	   System.out.println("A number:"+number);
 	   System.out.println("B i:"+b.i);
