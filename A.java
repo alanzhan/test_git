4075fc8b44906de8fad2b9972ccb3a32f0a163c5
alanzhan <zx_hfut@qq.com>
2015-07-20 09:17:04 +0800

    a.java

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A{
+	public static void main(String args[]){
+			int number=1;
+	    String name="alan";
+	    B b =new B();
+	   System.out.println("A number:"+number);
+	   System.out.println("B i:"+b.i);
+	}
+	
+}
